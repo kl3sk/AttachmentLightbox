bdef0f0761aee9a1fde0dbeb832313b6ba8020d4
Gregoire <klesk44@gmail.com>
2019-08-26 17:10:19 +0200

    First commit

---
+++ AttachmentLightbox.php
@@ -0,0 +1,55 @@
+<?php
+
+/** Plugin declaration
+ * extends MantisPlugin
+ * Example plugin that implements Jquery files
+ */
+
+class AttachmentLightboxPlugin extends MantisPlugin
+{
+    # Declare your plugin here
+    # Properties will be used by Mantis plugin's system
+    public function register()
+    {
+        $this->name = 'AttachementLightbox';
+        $this->description = 'Create lightbox to attached images';
+        $this->page = '';
+
+        $this->version = '0.0.1';
+        $this->requires = array(
+            "MantisCore" => "2.0.0",
+        );
+
+        $this->author = 'Kl3sk';
+        $this->contact = 'klesk44@gmail.com';
+        $this->url = '';
+    }
+
+    function hooks()
+    {
+        # retrieve existing hooks.
+        $hooks = parent::hooks();
+
+        # add in our plugin's hooks.
+        $hooks['EVENT_LAYOUT_RESOURCES'] = 'lightbox_css';
+        $hooks['EVENT_LAYOUT_PAGE_FOOTER'] = 'lightbox_scripts';
+        $hooks['EVENT_CORE_HEADERS'] = 'csp_headers';
+
+        return $hooks;
+    }
+
+    # This method will echo our '<script>'
+    public function lightbox_scripts()
+    {
+        # Implement the Jquery files
+        echo '<script type="text/javascript" src="' . plugin_file('lightgallery.js') . '"></script>';
+        echo '<script type="text/javascript" src="' . plugin_file('lightbox.js') . '"></script>';
+    }
+
+    # This method will echo our '<script>' link to Jquery
+    public function lightbox_css()
+    {
+        # Implement the Jquery files
+        echo '<link rel="stylesheet" type="text/css" href="' . plugin_file('lightgallery.css') . '"/>';
+    }
+}
---
+++ files/lightbox.js
@@ -0,0 +1,14 @@
+let selector = '.bug-attachment-preview-image';
+let wrapper = document.querySelector(selector);
+// wrapper.classList.add('gallery')
+// let children = wrapper.children;
+//
+// for (let child in children) {
+//     if (children.hasOwnProperty(child) && children[ child ].tagName === 'A') {
+//         console.log(children[ child ]);
+//         children[ child ].dataset.caption = "Image caption";
+//     }
+// }
+//
+
+lightGallery(wrapper)
