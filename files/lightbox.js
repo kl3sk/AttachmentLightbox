c4b93b302cffafc524054e81a18cdce4695bad29
Gregoire <klesk44@gmail.com>
2019-08-27 09:25:36 +0200

    Add lightGalley on activites bugnote

--- files/lightbox.js
+++ files/lightbox.js
@@ -1,14 +1,19 @@
-let selector = '.bug-attachment-preview-image';
-let wrapper = document.querySelector(selector);
-// wrapper.classList.add('gallery')
-// let children = wrapper.children;
-//
-// for (let child in children) {
-//     if (children.hasOwnProperty(child) && children[ child ].tagName === 'A') {
-//         console.log(children[ child ]);
-//         children[ child ].dataset.caption = "Image caption";
-//     }
-// }
-//
-
+// Creation
+let selector = '.bug-attachment-preview-image'
+let wrapper = document.querySelector(selector)
 lightGallery(wrapper)
+
+// Activities
+let bugNotes = document.querySelectorAll('.bugnote-note')
+for (let bugNote in bugNotes) {
+    if (bugNotes.hasOwnProperty(bugNote)) {
+        let links = bugNotes[ bugNote ].querySelectorAll('a[href^="file_download.php"]')
+        for (let link in links) {
+            if (links.hasOwnProperty(link)) {
+                lightGallery(links[ link ], {
+                    selector: 'this'
+                })
+            }
+        }
+    }
+}
